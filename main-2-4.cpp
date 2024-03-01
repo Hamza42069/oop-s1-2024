982bbff4f713d9c5603e8a6d650a16fb85e5963c
Hamza4657 <sicknice1@gmail.com>
2024-03-01 18:03:42 +1030

    tj the best

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -5,5 +5,5 @@ extern bool is_ascending(int array[], int n);
 int main (void)
 {
 	int array [4] = {1, 2 ,5, 2};
-	std::cout << "asscending order: " << is_ascending(array, 4) << std::endl
+	std::cout << "asscending order: " << is_ascending(array, 4) << std::endl;
 }
