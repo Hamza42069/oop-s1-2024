7284dae4157bb20ba10de0d34762b8280bc7a510
Hamza4657 <sicknice1@gmail.com>
2024-03-01 14:55:15 +1030

    tj the best

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -4,7 +4,7 @@ extern double array_mean(int array[], int n);
 
 int main(void)
 {
-	int array[6] = {1,2,3,4,6,8};
+	int array[6] = {1,2,3,4,6,2};
 	std::cout << "The average is: " << array_mean(array, 6) << std::endl;
 	return (0);
 }
