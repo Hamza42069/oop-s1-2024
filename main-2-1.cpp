83f02330322469787d82dea2a38f5e94d2834adc
Hamza4657 <sicknice1@gmail.com>
2024-03-01 15:29:48 +1030

    tj the best

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -5,6 +5,6 @@ extern min_element(int array[], int n);
 int	main(void)
 {
 	int array[6] = {9, 1, 3, 6, 7, 8}
-	std::cout << "min number:" min_element(array, 6) << std::endl;
+	std::cout << "min number:" << min_element(array, 6) << std::endl;
 	return (0);
 }
