a951dc4e0b30ef9dd9b815f0da5c7fbbeee6aa8a
Hamza4657 <sicknice1@gmail.com>
2024-03-01 18:34:40 +1030

    tj the best

--- main-2-5.cpp
+++ main-2-5.cpp
@@ -3,6 +3,6 @@
 extern is_descending(int array[], int n);
 int main (void)
 {
-	array[4] = {1,3,4,1};
+	int array[4] = {1,3,4,1};
 	std::cout << "descending order: " << is_descending(array, 4) << std::endl;
 }
